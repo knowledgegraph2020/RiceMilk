02a15bfbe6a7d96469abedd8de72689eaad19a1d
rookiePoo <jipeng93@163.com>
2020-02-07 17:37:41 +0800

    change sohu save dir

--- RiceMilk/SohuFinance/parse/config.py
+++ RiceMilk/SohuFinance/parse/config.py
@@ -1,5 +1,5 @@
-save_dir = '/Users/peng_ji/codeHub/rookieCode/RiceMilk/RiceMilk/SohuFinance/res'
+#save_dir = '/Users/peng_ji/codeHub/rookieCode/RiceMilk/RiceMilk/SohuFinance/res'
 
-#save_dir = '/data/files/'
+save_dir = '/data/files/'
 
 #save_dir = '/data/files/'+ end_dt + '/NetEaseFinance/international'
